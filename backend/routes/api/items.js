a181026fb0bde161fca5b3f66a513e1385b47628
anorrisOU <alex@alexnorris.com>
2022-08-12 21:30:38 -0400

    Fixing on item addition instead of recall

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -167,9 +167,6 @@ router.get("/:item", auth.optional, function (req, res, next) {
     .then(function (results) {
       var user = results[0];
 
-      req.item.image =
-        req.item.image === "" ? "/placeholder.png" : req.item.image;
-
       return res.json({ item: req.item.toJSONFor(user) });
     })
     .catch(next);
@@ -189,6 +186,8 @@ router.put("/:item", auth.required, function (req, res, next) {
 
       if (typeof req.body.item.image !== "undefined") {
         req.item.image = req.body.item.image;
+      } else {
+        req.item.image = "/placeholder.png";
       }
 
       if (typeof req.body.item.tagList !== "undefined") {
